e52c75e4b2de66c9b42776814628fdaca06eeaef
bidianqing <bidianqing@longfor.com>
2024-03-05 17:21:08 +0800

    *

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -10,18 +11,26 @@ import (
 var ctx = context.Background()
 
 func main() {
-	fmt.Println("Starting")
-
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
 
-	err := rdb.Set(ctx, "name", "tom", 0).Err()
+	err := rdb.Set(ctx, "name", "tom", time.Second*60).Err()
 	if err != nil {
-		panic(err)
+		fmt.Print(err)
 	}
 
-	fmt.Println("Success")
+	val, err := rdb.Get(ctx, "name").Result()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println("name", val)
+
+	age, err := rdb.Incr(ctx, "age").Result()
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println("name", age)
 }
